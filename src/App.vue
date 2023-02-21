c9ae4a0c9a82214ba73e52477b209f193b96d856
ablahum <mriztama@gmail.com>
2023-02-22 00:38:28 +0700

    refacto feature section

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,14 @@
   <HighlightsComp />
 
   <TestimonialsComp />
+
+  <FeatureComp />
+
+  <NewsletterComp />
 </template>
 
 <script>
-import { NavbarComp, HeaderComp, DealsComp, HighlightsComp, TestimonialsComp } from './components'
+import { NavbarComp, HeaderComp, DealsComp, HighlightsComp, TestimonialsComp, FeatureComp, NewsletterComp } from './components'
 
 export default {
   name: 'App',
@@ -21,20 +25,13 @@ export default {
     DealsComp,
     HighlightsComp,
     TestimonialsComp,
+    FeatureComp,
+    NewsletterComp,
   },
 }
 </script>
 
 <style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
-
 @import url('https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@500;600;700&family=Inter:wght@500;700&family=Roboto:wght@400;500;700&display=swap');
 
 @tailwind base;
---
+++ src/components/FeatureComp.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="sm:container mx-4 sm:mx-auto lg:py-14 py-12 flex lg:flex-row flex-col items-center justify-center">
+    <div>
+      <img
+        src="../assets/feature.png"
+        alt="feature"
+      />
+    </div>
+
+    <div class="lg:ml-12 mt-12">
+      <h2 class="font-bai font-bold text-5xl">Handoff your work smarter now</h2>
+
+      <p class="text-text font-roboto mt-6">Create documentation for the collaborators (i.e. designers or devs) directly in your design file.</p>
+
+      <div class="mt-8">
+        <div
+          v-for="(item, i) in items"
+          :key="i"
+          class="p-4 flex items-center"
+        >
+          <div class="w-1/4">
+            <img
+              :src="item.icon"
+              alt="feature icon"
+              class="w-full h-full"
+            />
+          </div>
+
+          <div class="ml-4 w-9/12">
+            <h3 class="font-bai font-bold">{{ item.title }}</h3>
+
+            <p class="mt-4 font-roboto text-text">{{ item.subtitle }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import strategic from '../assets/strategic.svg'
+import schedule from '../assets/schedule.svg'
+
+export default {
+  name: 'FeatureComp',
+
+  data() {
+    return {
+      items: [
+        {
+          icon: strategic,
+          title: 'Strategic',
+          subtitle: 'Suggests that the component spacing between cards and elements',
+        },
+        {
+          icon: schedule,
+          title: 'Work Schedule',
+          subtitle: 'Work schedule is the time an employee is expected to be on the job',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
